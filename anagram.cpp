4ea75e6b1235f65b3059c137a2a391776ba287e5
tanisha083 <tanisha083btece21@igdtuw.ac.in>
2022-11-15 22:59:45 +0530

    Fourth commit

---
+++ anagram.cpp
@@ -0,0 +1,17 @@
+https://leetcode.com/problems/valid-anagram/
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        string *sp=&s;
+        string *tp=&t;
+        sort(sp->begin(),sp->end());
+        sort(tp->begin(),tp->end());
+        if (s==t){
+            return true;
+           }
+        else{
+            return false;
+        }
+        
+    }
+};
