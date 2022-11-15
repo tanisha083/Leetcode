0a2e9e83d203e9e3058334508acb41540bbf448c
tanisha083 <tanisha083btece21@igdtuw.ac.in>
2022-11-15 22:57:21 +0530

    Fourth commit

---
+++ Buyandsellstocks.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/748691823/
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxcur=0;
+        int max_so_far=0;
+        for(int i=1;i<prices.size();i++){
+            maxcur=max(0,maxcur+=prices[i]-prices[i-1]);
+            max_so_far=max(maxcur,max_so_far);    
+            }
+        
+        
+        return max_so_far ;
+    }
+};
