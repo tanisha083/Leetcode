c2ba7a3326840b602dcf4d6e7c99aecaf9f8b039
tanisha083 <tanisha083btece21@igdtuw.ac.in>
2023-01-13 23:20:24 +0530

    program30

---
+++ containsDuplicate2.cpp
@@ -0,0 +1,10 @@
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        return nums.size() > set<int>(nums.begin(), nums.end()).size();        
+    }
+};
+// https://leetcode.com/problems/contains-duplicate/submissions/784973140/
